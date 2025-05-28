4268f5e5c128e23fc60b6ad6bc175ee40d35e7b7
daniel-kav <dankavatha01@gmail.com>
2025-05-28 09:28:06 +0300

    feat(users): implement user entity and CRUD operations
    - Add TypeORM module and User entity to users module
    - Create validation decorators for CreateUserDto
    - Implement user repository methods in UsersService

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+}
