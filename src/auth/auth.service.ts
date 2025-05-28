6ad7e7cf73a2d45f801b577d638bb81624e0c44b
daniel-kav <dankavatha01@gmail.com>
2025-05-29 01:07:28 +0300

    feat(auth): implement registration and login functionality
    - Rename CreateAuthDto to RegisterAuthDto
    - Add register method to handle user registration
    - Implement login method for user authentication
    - Throw exceptions for existing users and invalid credentials

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateAuthDto } from './dto/register-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Auth } from './entities/auth.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { LoginUserDto } from 'src/users/dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ){}
+
+  async register(createAuthDto: CreateAuthDto) {
+    const existingUser = await this.usersService.findByEmail(createAuthDto.email);
+    if (existingUser) throw new ConflictException('Email already in use 😒');
+    const user = await this.usersService.create(createAuthDto);
+    return user
+
+  }
+  async login(loginDto: LoginUserDto) {
+    const user = await this.usersService.findByEmail(loginDto.email);
+    console.log(user)
+    if (!user || !(await user.comparePassword(loginDto.password))) {
+      throw new UnauthorizedException('Invalid credentials !!😁');
+    }
+    const payload = {sub: user.id, email: user.email, role: user.role};
+    const token = await this.jwtService.signAsync(payload);
+    return {
+      access_token: token,
+      user
+    };
   }
 
   findAll() {
