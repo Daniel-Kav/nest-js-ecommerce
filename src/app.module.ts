558de1b03c825bfe627f00a9e781aa7ffd575189
daniel-kav <dankavatha01@gmail.com>
2025-06-03 20:08:15 +0300

    feat(caching): add redis and in-memory caching with interceptors
    Implement caching mechanism using both Redis and in-memory stores with CacheModule.
    Added global CacheInterceptor to automatically cache responses.
    This improves performance by reducing database queries for frequently accessed data.

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,37 @@ import { ReviewsModule } from './reviews/reviews.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/db.config';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { createKeyv, Keyv } from '@keyv/redis';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CacheableMemory } from 'cacheable';
 
 @Module({
   imports: [
+    // CacheModule.register({
+    //   isGlobal: true,
+    //   ttl: 5000,
+    //   // store: createKeyv('redis://localhost:6379')
+    // }),
     TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule, UsersModule, ProductsModule, CategoriesModule, OrdersModule, PaymentsModule, CartModule, ReviewsModule],
+    AuthModule, UsersModule, ProductsModule, CategoriesModule, OrdersModule, PaymentsModule, CartModule, ReviewsModule,
+    CacheModule.registerAsync({
+      useFactory: async () => {
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+            }),
+            createKeyv('redis://localhost:6379'),
+          ],
+        };
+      },
+    }),
+  ],
   controllers: [],
-  providers: [      
+  providers: [ {
+    provide: APP_INTERCEPTOR,
+    useClass: CacheInterceptor,}     
   ],
 })
 export class AppModule {}
